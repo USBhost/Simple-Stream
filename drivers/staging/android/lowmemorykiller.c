2031908c1580f5f833b14e42ca3fe0910b0d7c21
Krishna Reddy <vdumpa@nvidia.com>
2015-02-25 20:19:12 -0800

    android: lowmemorykiller: account other_file memory precisely
    account only reclaimable memory under other_file memory.
    Bug 200055047
    
    Change-Id: If2b48eb77cda58e2e681fd7233c7a5181bcbc713
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/711848
    (cherry picked from commit e33b6072f90824daa048a51f2e586e66146022ac)
    Reviewed-on: http://git-master/r/711473
    Reviewed-by: Maneet Maneet Singh <mmaneetsingh@nvidia.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -91,8 +91,12 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			 + nvmap_page_pool_get_unused_pages()
 #endif
 			 ;
-	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
+	int other_file = global_page_state(NR_FILE_PAGES)
+			 - global_page_state(NR_FILE_MAPPED) -
+			global_page_state(NR_SHMEM);
+
+	if (other_file < 0)
+		other_file = 0;
 
 	si_swapinfo(&swap_info);
 	other_free += swap_info.freeswap;
